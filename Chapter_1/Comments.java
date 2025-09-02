1912dabe86322f7e877b1561617a75e7ffef865c
DirknashParame-UCM <dirknash2@gmail.com>
2025-09-02 17:35:22 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,20 @@
+public class Comments {
+
+    public static void main(String[] args) {
+    
+         /**
+         * Simple Java program with different types of comments.
+         */
+
+
+        // Single-line comment: print a message
+        System.out.println("Hello, World!");
+
+        /*
+         * Multi-line comment:
+         * The next line prints a second message.
+         */
+       
+
+    }
+}
