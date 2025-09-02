e90e943db335e91333efc2f1538fb17167010e7b
DirknashParame-UCM <dirknash2@gmail.com>
2025-09-02 17:21:34 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,14 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Thru These Tears [LANY]");
+        System.out.println("You said it was over");
+        System.out.println("But when we got closer");
+        System.out.println("You cried on my shoulder");
+        System.out.println("Goodbye");
+        System.out.println("In the end I'm gonna be alright");
+        System.out.println("But it might take a hundred sleepless nights");
+        System.out.println("To make the memories of you disappear");
+        System.out.println("Well, right now I can't see nothing through these tears");
+    }
+}
+
